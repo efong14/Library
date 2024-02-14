bb79eda498f4acbd228ba89e4e16d9b9fde3b237
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-02-14 18:00:16 +0800

    Added for in loop to display book object in HTML.

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-const author = document.querySelector('.author')
-const title = document.querySelector('.title')
-const pages = document.querySelector('.pages')
-const read = document.querySelector('.read')
+const card = document.querySelector('.card')
+
 
 const myLibrary = [];
 
 function Book (author, title, pages, status) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = status;
+    this.Author = author;
+    this.Title = title;
+    this.Pages = pages;
+    this.Read = status;
     addBookToLibrary (this);
 }
 
 function addBookToLibrary (newBook) {
-    myLibrary.push (newBook)
+    myLibrary.push (newBook);
     myLibrary.forEach(book => {
         console.log(book.read);
     });
     myLibrary.forEach(book => {
-        author.textContent = book.author;
-        title.textContent = book.title;
-        pages.textContent = book.pages;
-        read.textContent = book.read;
-    })
-
+        for (const key in book) {
+            let item = document.createElement('div');
+            card.appendChild(item);
+            item.classList.add (key);
+            item.textContent = key + ': ' + book[key];
+            if (item.textContent == 'Read: Yes') {
+                item.classList.add ('yes')
+            } else if (item.textContent == 'Read: No' || 'Read: no') {
+                item.classList.add ('no')
+            }
+        };
+    });
 }
+
+
+
+const theHobbit = new Book ('J.R.R Tolkien', 'The Hobbit', '295 pages', 'No');
+
 
