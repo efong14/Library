236a81a6c35f914839e43d73f0956367ea634baf
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-02-26 18:04:29 +0800

    Used findIndex to remove book from library upon clicking delete button.

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ const authorIn = document.getElementById ('author');
 const titleIn = document.getElementById ('title');
 const pagesIn = document.getElementById ('pages');
 
-
-
-
 const myLibrary = [];
+
+
+openModal.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+submit.addEventListener('click', (e) => {
+    // e.preventDefault();
+    // dialog.close()
+    new Book (authorIn.value, titleIn.value, pagesIn.value, radio.value);
+    displayLibrary();
+})
 
 function Book (author, title, pages, status) {
     this.Author = author;
@@ -24,30 +33,49 @@ function Book (author, title, pages, status) {
 function addBookToLibrary (newBook) {
     myLibrary.push (newBook);
     let newCard = document.createElement ('div');
-    newCard.classList.add('card')
-    cardContainer.appendChild(newCard)
+    newCard.classList.add('card');
+    newCard.setAttribute('booknum', (myLibrary.length - 1));
+    cardContainer.appendChild(newCard);
     for (const key in newBook) {
         let item = document.createElement('div');
         newCard.appendChild(item);
         item.classList.add (key); 
         item.textContent = key + ': ' + newBook[key];
-        if (item.textContent == 'Read: Yes') {
-            item.classList.add ('yes');
-        } else if (item.textContent == 'Read: No' || 'Read: no') {
-            item.classList.add ('no');
-        };
     };
+    // FIGURE OUT HOW TO REMOVE FROM MYLIBRARY AFTER THE FIRST REMOVAL
+    let removeBook = document.createElement ('button');
+    removeBook.textContent = 'Remove book';
+    newCard.appendChild(removeBook);
+    removeBook.addEventListener('click', () => {
+        newCard.remove();
+        console.log (newBook.Author)
+        let indexed = myLibrary.findIndex((book) => book.Author == newBook.Author)
+        myLibrary.splice(indexed, 1)
+    });
 };
 
-openModal.addEventListener('click', () => {
-    dialog.showModal();
-});
-
-submit.addEventListener('click', (e) => {
-    // e.preventDefault();
-    // dialog.close()
-    new Book (authorIn.value, titleIn.value, pagesIn.value, radio.value);
-})
+// function displayLibrary() {
+//     myLibrary.forEach(book => {
+//         let newCard = document.createElement ('div');
+//         newCard.classList.add('card');
+//         newCard.setAttribute('booknum', (myLibrary.indexOf(book)));
+//         cardContainer.appendChild(newCard);
+//         for (const key in book) {
+//             let item = document.createElement('div');
+//             newCard.appendChild(item);
+//             item.classList.add (key); 
+//             item.textContent = key + ': ' + book[key];
+//         };
+//         let removeBook = document.createElement ('button');
+//         removeBook.textContent = 'Remove book';
+//         newCard.appendChild(removeBook);
+//         removeBook.addEventListener('click', () => {
+//             allCards.remove();
+//             console.log(allCards);
+//             // myLibrary.splice(newCard.getAttribute('booknum'), 1);
+//         });
+//     });
+// }
 
 
 // test:
