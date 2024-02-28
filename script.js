20cf427518266d529d11cd90682a16d8949ef0c2
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-02-28 17:16:37 +0800

    Replaced loop to display entried with a function to be able to set read toggle button as the read status entry. Added read toggle button. Added form reset function on submit.

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const dialog = document.querySelector('dialog');
 const openModal = document.querySelector('.openModal');
 const submit = document.querySelector('.submit');
 const cancel = document.querySelector('.cancel');
+const frm = document.querySelector('form');
 const authorIn = document.getElementById ('author');
 const titleIn = document.getElementById ('title');
 const pagesIn = document.getElementById ('pages');
-const readVal = document.getElementById ('readStatus')
+const readVal = document.getElementById ('readStatus');
 
 const myLibrary = [];
 
@@ -16,10 +17,11 @@ openModal.addEventListener('click', () => {
 });
 
 submit.addEventListener('click', (e) => {
-    // e.preventDefault();
-    // dialog.close()
+    e.preventDefault();
+    dialog.close();
     readVal.checked == true ? readIn = 'Yes' : readIn = 'No';
     new Book (authorIn.value, titleIn.value, (pagesIn.value + ' pages'), readIn);
+    frm.reset();
 })
 
 function Book(author, title, pages, status) {
@@ -31,21 +33,44 @@ function Book(author, title, pages, status) {
 }
 
 function addBookToLibrary(newBook) {
-    myLibrary.push (newBook);
-    displayBook (newBook)
+    myLibrary.push(newBook);
+    displayBook(newBook);
 }
 
 function displayBook(newBook) {
     let newCard = document.createElement('div');
     newCard.classList.add('card');
     cardContainer.appendChild(newCard);
-    for (const key in newBook) {
-        let item = document.createElement('div');
-        newCard.appendChild(item);
-        item.classList.add(key); 
-        item.textContent = key + ': ' + newBook[key];
-    };
+    displayEntry(newCard, 'Author', newBook.Author);
+    displayEntry(newCard, 'Title', newBook.Title);
+    displayEntry(newCard, 'Pages', newBook.Pages);
+    readBtn(newCard, newBook);
     deleteBtn(newCard, newBook);
+}
+
+function displayEntry (newCard, itemClass, itemContent) {
+    let item = document.createElement('div');
+    newCard.appendChild(item);
+    item.classList.add(itemClass); 
+    item.textContent = itemClass + ': ' + itemContent;
+}
+
+function readBtn (newCard, newBook) {
+    let toggleRead = document.createElement ('button');
+    toggleRead.classList.add('Read')
+    toggleRead.textContent = 'Read: ' + newBook.Read;
+    newCard.appendChild(toggleRead);
+    toggleRead.addEventListener('click', () => {
+        if(newBook.Read == 'Yes') {
+            newBook.Read = 'No';
+            toggleRead.textContent = 'Read: No';
+            toggleRead.classList = 'Read No'
+        } else {
+            newBook.Read = 'Yes';
+            toggleRead.textContent = 'Read: Yes';
+            toggleRead.classList = 'Read Yes'
+        };
+    });
 }
 
 function deleteBtn(newCard, newBook) {    
@@ -59,20 +84,11 @@ function deleteBtn(newCard, newBook) {
     });
 }
 
-//     // Read Toggle
-//     let toggleRead = document.createElement ('button');
-//     toggleRead.textContent = 'Read';
-//     newCard.appendChild(toggleRead);
-//     toggleRead.addEventListener('click', () => {
-//         newBook.Read = "No"
-//         console.log(newBook.Read)
-//     })
-// };
 
-// test:
+// Books on load:
 var long = new Book("Peter Weiss ", "The Persecution and Assassination of Jean-Paul Marat as Performed by the Inmates of the Asylum of Charenton Under the Direction of the Marquis de Sade", "896 pages", "No");
 var dune = new Book("Frank Herbert", "Dune", "896 pages", "No");
-var theHobbit = new Book("J.R.R. Tolkien", "The Hobbsit", "295 pages", "Yes");
+var theHobbit = new Book("J.R.R. Tolkien", "The Hobbit", "295 pages", "Yes");
 
 
 
